8adc46e1db88e37f76cc59dc47f4fbd9ccc6e6a5
Kevin Heredia <kevin@ravecodesolutions.com>
2025-03-19 19:25:19 -0500

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Kevin Heredia on 19/3/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
